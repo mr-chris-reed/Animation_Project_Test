a598ad563327a6fceab81e777990529282399d8e
root <chrree@bhasd.org>
2022-03-28 13:48:39 -0400

    first commit

---
+++ controller.sh
@@ -0,0 +1,18 @@
+while :;
+  do
+    cd intro_slide
+    python main.py
+    cd ..
+    cd twocats
+    python main.py
+    cd ..
+    cd flower
+    python main.py
+    cd ..
+    cd star_wars
+    python main.py
+    cd ..
+    cd pyramids
+    python main.py
+    cd ..
+  done
